1c2c32c9d6ed78e03dc2babc8cb5788ebf85123c
Murilo Alves <murilo.udi@gmail.com>
2021-08-18 23:09:43 +0200

    Update index.jsx

--- src/views/new/index.jsx
+++ src/views/new/index.jsx
@@ -8,7 +8,7 @@ import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const NewBlogPost = () => {
   const [post, setPost] = useState({});
-  const [imgUrl, setImgUrl] = useState();
+  const [imgUrl, setImgUrl] = useState({});
   const [imgToSend, setImgToSend] = useState("");
 
   const handleChange = (key, value) => {
